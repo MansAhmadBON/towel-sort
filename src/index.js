a0e9053c9d9db499eab733a82d2c04b085b5d200
MansAhmadBONgit config --global user.name MansAhmadBONgit config --global user.name MansAhmadBON <mansue.ahmad.94@mail.ru>
2020-02-21 11:06:52 +0300

    feat: added solution

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix) {
+    if (!Array.isArray(matrix) || matrix.length === 0) {
+        return [];
+    } else {
+        var resultArr = [];
+        for (let i = 0; i < matrix.length; i++) {
+            let currentArr = matrix[i];
+            if (i % 2 === 0) {
+                resultArr.push(currentArr);
+            } else {
+                let sortCurrentArr = currentArr.sort((a, b) => b - a);
+                resultArr.push(sortCurrentArr);
+            }
+        }
+        let str = resultArr.toString().replace(/[^-0-9]/gim, " ");
+        return str.split(" ").map(e => Number(e));
+    }
+};
